d48a19b9383b350a260cad7f8e2d5bedd8527d8c
Luiz Henrique <luiz.ssantos@outlook.com.br>
2024-01-30 05:44:52 -0300

    first3 commit

--- main.js
+++ main.js
@@ -5,64 +5,99 @@ $(document).ready(function () {
   $("#cpf").mask("000.000.000-00");
   $("#cep").mask("00000-000");
 
-  // Manipula o clique no botão de cadastrar
-  $("#butaoCadastrar").click(function (e) {
-    // Impede o comportamento padrão do formulário
-    e.preventDefault();
-
-    // Obtém os valores dos campos
-    let nome = $("#nomeCompleto").val();
-    let email = $("#eMail").val();
-    let telefone = $("#telefone").val();
-    let cpf = $("#cpf").val();
-    let endereco = $("#enderecoCompleto").val();
-    let cep = $("#cep").val();
-
-    // Obtém a hora atual
-    let horaAtual = new Date();
-    let horas = horaAtual.getHours();
-    let minutos = horaAtual.getMinutes();
-    let segundos = horaAtual.getSeconds();
-
-    // Formata a hora completa
-    let horaCompleta = `${horas}:${minutos}:${segundos}`;
-
-    // Verifica se todos os campos foram preenchidos
-    if (
-      nome === "" ||
-      email === "" ||
-      telefone === "" ||
-      cpf === "" ||
-      endereco === "" ||
-      cep === ""
-    ) {
-      // Exibe mensagem de cadastro pendente
-      $("body #msgFinal").css("display", "block");
-      $("body #msgFinal").append(
-        `<h3>Cadastro Pendente.</h3><h4>(Preencha todos os campos) <i>${horaCompleta}</i></h4>`
-      );
-      $("body #msgFinal h3, i").css("color", "red");
-    } else {
+  // Inicializa o plugin de validação
+  $("#formComentario").validate({
+    errorClass: "error", // Define a classe de erro usada pelo plugin
+    rules: {
+      nomeCompleto: {
+        required: true,
+        minlength: 4,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      telefone: {
+        required: true,
+        minlength: 15,
+      },
+      cpf: {
+        required: true,
+        minlength: 14,
+      },
+      enderecoCompleto: {
+        required: true,
+        minlength: 5,
+      },
+      cep: {
+        required: true,
+        minlength: 9,
+      },
+    },
+    messages: {
+      nomeCompleto: {
+        required: "Digite o seu nome",
+        minlength: "O seu nome deve ter pelo menos 4 letras",
+      },
+      email: {
+        required: "Digite o seu e-mail para contato",
+        email: "Digite um e-mail válido",
+      },
+      telefone: {
+        required: "Digite o seu telefone",
+        minlength: "O seu telefone deve ter pelo menos 9 números",
+      },
+      cpf: {
+        required: "Digite o seu CPF",
+        minlength: "O seu CPF deve ter pelo menos 11 números",
+      },
+      enderecoCompleto: {
+        required: "Digite o seu endereço",
+        minlength: "O seu endereço deve ter pelo menos 5 caracteres",
+      },
+      cep: {
+        required: "Digite o seu CEP",
+        minlength: "O seu CEP deve ter pelo menos 9 números",
+      },
+    },
+    // Função chamada em caso de sucesso
+    submitHandler: function (form) {
       // Exibe mensagem de cadastro concluído
-      $("body #msgFinal").css("display", "block");
-      $("body #msgFinal").append(
-        `<h3>Cadastro Concluído</h3><i>${horaCompleta}</i>`
-      );
+      $("#msgFinal").css("display", "block");
+      $("#msgFinal").html(`<h3>Cadastro Concluído</h3>`);
+
+      // Obtém a hora atual
+      let horaAtual = new Date();
+      let horas = horaAtual.getHours();
+      let minutos = horaAtual.getMinutes();
+      let segundos = horaAtual.getSeconds();
+
+      // Formata a hora completa
+      let horaCompleta = `${horas}:${minutos}:${segundos}`;
+
+      // Exibe a hora no formato HH:mm:ss
+      $("#msgFinal").append(`<i>${horaCompleta}</i>`);
 
       // Exibe os dados do cadastro
-      $("body #msgFinal").append(`<p><strong>Nome: </strong>${nome}</p>`);
-      $("body #msgFinal").append(`<p><strong>E-mail: </strong>${email}</p>`);
-      $("body #msgFinal").append(
-        `<p><strong>Telefone: </strong>${telefone}</p>`
+      $("#msgFinal").append(
+        `<p><strong>Nome: </strong>${$("#nomeCompleto").val()}</p>`
       );
-      $("body #msgFinal").append(`<p><strong>CPF: </strong>${cpf}</p>`);
-      $("body #msgFinal").append(
-        `<p><strong>Endereço Completo: </strong>${endereco}</p>`
+      $("#msgFinal").append(
+        `<p><strong>E-mail: </strong>${$("#eMail").val()}</p>`
       );
-      $("body #msgFinal").append(`<p><strong>CEP: </strong>${cep}</p>`);
+      $("#msgFinal").append(
+        `<p><strong>Telefone: </strong>${$("#telefone").val()}</p>`
+      );
+      $("#msgFinal").append(`<p><strong>CPF: </strong>${$("#cpf").val()}</p>`);
+      $("#msgFinal").append(
+        `<p><strong>Endereço Completo: </strong>${$(
+          "#enderecoCompleto"
+        ).val()}</p>`
+      );
+      $("#msgFinal").append(`<p><strong>CEP: </strong>${$("#cep").val()}</p>`);
 
       // Limpa os campos de entrada
-      $("input").val("");
-    }
+      form.reset();
+    },
   });
 });
